aa6b35a741ad7d164e24a84d086d093c3e3a1469
Thomas Portelange <thomas.portelange@boomerang.be>
2014-12-18 14:45:58 +0100

    finalise builder

--- javascript/map-builder.js
+++ javascript/map-builder.js
@@ -5,22 +5,24 @@ function buildLeafletMap(id) {
 	var lat = $map.data('lat');
 	var lon = $map.data('lon');
 	var zoom = $map.data('zoom');
+	if(!zoom) {
+		zoom = 12;
+	}
 	var tilelayer = $map.data('tilelayer');
 	var tileOptions = $map.data('tileoptions');
 
 	var map = L.map(id, mapOptions);
-
+	
 	//add tiles
 	L.tileLayer(tilelayer, tileOptions).addTo(map);
-
+	
 	map.setView(new L.LatLng(lat, lon), zoom);
 
 	var marker = L.marker([lat, lon]);
-//	marker.addTo(map);
+	marker.addTo(map);
 	if ($content.length) {
-//		console.log($content);
-//		marker.bindPopup($content.html())
-//				.openPopup();
+		marker.bindPopup($content.html())
+				.openPopup();
 	}
 
 }
