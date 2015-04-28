753a39300fe606ca4d6f50af236bbe47b92b60ef
Thomas Portelange <thomas.portelange@boomerang.be>
2015-04-28 16:09:00 +0200

    auto configure prepends providers

--- _config.php
+++ _config.php
@@ -6,10 +6,10 @@ if(!defined('GEOTOOLS_PATH')) define('GEOTOOLS_PATH', rtrim(basename(dirname(__F
 $baseProviders = Config::inst()->get('Geocoder', 'providers');
 
 if (defined('BING_MAPS_API_KEY')) {
-    $baseProviders['BingMaps'] = array(BING_MAPS_API_KEY, 'locale');
+    $baseProviders = array('BingMaps' => array(BING_MAPS_API_KEY, 'locale')) + $baseProviders;
 }
 if (defined('GOOGLE_MAPS_API_KEY')) {
-    $baseProviders['GoogleMaps'] = array('locale', null, true, GOOGLE_MAPS_API_KEY);
+    $baseProviders = array('GoogleMaps' => array('locale', null, true, GOOGLE_MAPS_API_KEY)) + $baseProviders;
 }
 
 Config::inst()->update('Geocoder', 'providers', $baseProviders);
